7dad9c78fd75da4fa0ec37d8973048a05e5306bf
Amit <bendale.amit7@gmail.com>
2018-01-27 11:58:11 +0530

    Express session with basic authentication

--- app.js
+++ app.js
@@ -52,40 +52,27 @@ app.use(session({
   store: new FileStore()
 }))
 
+app.use('/', index);
+app.use('/users', users);
+
+
 function auth(req,res,next){
   console.log("AUthorizing...")
   console.log(req.session);
 
   if(!req.session.user){
-    var authHeaders = req.headers.authorization;
-
-    if(!authHeaders){
       var err = new Error("You are not authenticated");
       res.setHeader('WWW-Authenticate','Basic');
-      err.status = 401;
+      err.status = 403;
       return next(err);
-    }
-
-    var auth = new Buffer(authHeaders.split(' ')[1],'base64').toString().split(':');
-    var userName = auth[0];
-    var password = auth[1];
-
-    if(userName === 'admin' && password === 'password'){
-      req.session.user = 'admin';
-      next();
-    }else {
-      var err = new Error("You are not authenticated");
-      res.setHeader('WWW-Authenticate','Basic');
-      err.status = 401;
-      next(err);
-    }
-  }else {
-    if(req.session.user === 'admin'){
+  }  
+  else {
+    if(req.session.user === 'authenticated'){
       next();
     }else{
       var err = new Error("You are not authenticated");
       res.setHeader('WWW-Authenticate','Basic');
-      err.status = 401;
+      err.status = 403;
       return next(err);
     }
   }
@@ -97,8 +84,7 @@ app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
