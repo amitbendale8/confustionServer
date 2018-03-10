5a5bfaa510ccb3d1e9f1d0f3e228109acb46b835
Amit <bendale.amit7@gmail.com>
2018-03-10 11:24:11 +0530

    File UPload

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var leaderRouter = require('./routes/leaderRouter');
+var uploadRouter = require('./routes/uploadRouter');
 var promoRouter = require('./routes/promoRouter');
 var passport = require('passport');
 var authenticate = require('./authenticate')
@@ -70,6 +71,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
+app.use('/imageUpload',uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
