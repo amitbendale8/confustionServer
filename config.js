2df3d3504078dcd11ea6c27a71e2f0a96ea35ea6
Amit <bendale.amit7@gmail.com>
2018-03-10 22:48:16 +0530

    Facebook token

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook' : {
+        clientID:'830011970518832',
+        clientSecret:"55674dfc8273a13daf6b196bf9eb01ab"
+    }
 }
